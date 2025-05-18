08d8fea951eea08fc63bffd9db47b0095138bf4d
SayfullaevSirojiddin <-email@example.com>
2025-05-18 16:10:22 +0500

    commit

--- src/pages/activityPages/theMostImportantResearchResultsPage/items/section/Section.jsx
+++ src/pages/activityPages/theMostImportantResearchResultsPage/items/section/Section.jsx
@@ -1,19 +1,52 @@
-import React, { useContext, useEffect } from 'react';
-import styles from './section.module.scss'; // Убедитесь, что файл section.module.scss существует
+import React, { useContext } from 'react';
+import styles from './section.module.scss';
 import { LanguageContext } from '../../../../../context/LanguageContext';
-
-
 
 export default function Section() {
   const { language } = useContext(LanguageContext);
 
-
+  const data = [
+    {
+      title: {
+        uz: "O‘zbekistonning tosh davri madaniyati",
+        ru: "Культура каменного века Узбекистана",
+        en: "",
+      },
+      text: [
+        {
+          uz: "",
+          ru: "Получены новые археологические и палеоантропологические материалы, раскрывающие древнейшую историю народов Узбекистана и Центральной Азии в целом.",
+          en: "",
+        },
+        {
+          uz: "",
+          ru: "С открытием и исследованием ферганотропов история заселения Центральной Азии была удревнена на 1,5 млн. лет...",
+          en: "",
+        },
+        {
+          uz: "",
+          ru: "В области изучения палеолитических стоянок Кульбулак, Кутурбулак, Зирабулак проведена четкая технико-типологическая классификация технокомплексов...",
+          en: "",
+        },
+      ],
+    },
+  ];
 
   return (
     <section className={styles.container}>
-      
-      акаак
-          
+      {data.map((item, index) => (
+        <div key={index}>
+          <h1>{item.title[language]}</h1>
+
+          <ul>
+            {item.text.map((textItem, i) => (
+              <li key={i}>
+                <p>{textItem[language]}</p>
+              </li>
+            ))}
+          </ul>
+        </div>
+      ))}
     </section>
   );
 }

1bc947653106a3501523eb1448c074e0e7f6a198
SayfullaevSirojiddin <-email@example.com>
2025-06-07 03:41:09 +0500

    commit

--- src/api/api.js
+++ src/api/api.js
@@ -1,6 +1,6 @@
-const api = 'http://localhost:3109'
+// const api = 'http://localhost:3109'
 
-// const api = "https://milliy-arxeologiya-markazi-admin-api.onrender.com"
+const api = "https://milliy-arxeologiya-markazi-admin-api.onrender.com"
 
 
 

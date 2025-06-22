758034f4ed392b4e0f869fc6a21297bc45701174
SayfullaevSirojiddin <-email@example.com>
2025-06-22 22:26:11 +0500

    commit

--- src/pages/homePage/items/news/News.jsx
+++ src/pages/homePage/items/news/News.jsx
@@ -1,158 +1,168 @@
-import React, { useContext } from 'react'
+import React, { use, useContext, useEffect, useState } from 'react'
 import './news.scss'
 
 import { LanguageContext } from '../../../../context/LanguageContext';
-import { Link } from 'react-router-dom';
+import { Await, Link } from 'react-router-dom';
 
+
+import axios from 'axios';
+import getApiUrl from '../../../../api/api';
 
 
 
 export default function News() {
+    const [newsListData, setNewsListData] = useState([]);
+
+    const [news, setNews] = useState([]);
+    const [ads, setAds] = useState([]);
+    const [events, setEvents] = useState([]);
+    
+
+    const dataType = [
+            {
+                title: {
+                    uz: "Yangiliklar",
+                    ru: "Новости",
+                    en: "News"
+                },
+                link: '/news',
+                },
+            {
+                title: {
+                    uz: "E'lonlar",
+                    ru: "Объявления",
+                    en: "Ads"
+                },
+                link: '/ads',
+            },
+            {
+                title: {
+                    uz: "Tadbirlar",
+                    ru: "События",
+                    en: "Events"
+                },
+                link: '/events',
+            }
+        ]
+
 
     const { language } = useContext(LanguageContext);
 
-    let data = [
-        {
-            title: {
-                uz:"O‘zbekiston florasi laboratoriyasi xodimlari Qarshi davlat universitetida ilmiy-amaliy seminar o‘tkazdi",
-                ru: "Сотрудники лаборатории флоры Узбекистана провели научно-практический семинар в Каршинском государственном университете",
-                en: "Employees of the Uzbekistan Flora Laboratory held a scientific and practical seminar at Karshi State University",
-            },
-            data: "07.04.2025",
-            img: 'https://picsum.photos/id/1015/400/800',
-            link: '/uz/news/1',
-        },
-        {
-            title: {
-                uz:"O‘zbekiston florasi laboratoriyasi xodimlari Qarshi davlat universitetida ilmiy-amaliy seminar o‘tkazdiz",
-                ru: "Сотрудники лаборатории флоры Узбекистана провели научно-практический семинар в Каршинском государственном университете",
-                en: "Employees of the Uzbekistan Flora Laboratory held a scientific and practical seminar at Karshi State University",
-            },
-            data: "07.04.2025",
-            img: 'https://picsum.photos/id/1015/400/800',
-            link: '/uz/news/1',
-        },
-        {
-            title: {
-                uz:"O‘zbekiston florasi laboratoriyasi xodimlari Qarshi davlat universitetida ilmiy-amaliy seminar o‘tkazdi",
-                ru: "Сотрудники лаборатории флоры Узбекистана провели научно-практический семинар в Каршинском государственном университете",
-                en: "Employees of the Uzbekistan Flora Laboratory held a scientific and practical seminar at Karshi State University",
-            },
-            data: "07.04.2025",
-            img: 'https://picsum.photos/id/1015/400/800',
-            link: '/uz/news/1',
-        },
-        {
-            title: {
-                uz:"O‘zbekiston florasi laboratoriyasi xodimlari Qarshi davlat universitetida ilmiy-amaliy seminar o‘tkazdi",
-                ru: "Сотрудники лаборатории флоры Узбекистана провели научно-практический семинар в Каршинском государственном университете",
-                en: "Employees of the Uzbekistan Flora Laboratory held a scientific and practical seminar at Karshi State University",
-            },
-            data: "07.04.2025",
-            img: 'https://picsum.photos/id/1015/400/800',
-            link: '/uz/news/1',
-        },
-        {
-            title: {
-                uz:"O‘zbekiston florasi laboratoriyasi xodimlari Qarshi davlat universitetida ilmiy-amaliy seminar o‘tkazdi",
-                ru: "Сотрудники лаборатории флоры Узбекистана провели научно-практический семинар в Каршинском государственном университете",
-                en: "Employees of the Uzbekistan Flora Laboratory held a scientific and practical seminar at Karshi State University",
-            },
-            data: "07.04.2025",
-            img: 'https://picsum.photos/id/1015/400/800',
-            link: '/uz/news/1',
-        },
-        {
-            title: {
-                uz:"O‘zbekiston florasi laboratoriyasi xodimlari Qarshi davlat universitetida ilmiy-amaliy seminar o‘tkazdi",
-                ru: "Сотрудники лаборатории флоры Узбекистана провели научно-практический семинар в Каршинском государственном университете",
-                en: "Employees of the Uzbekistan Flora Laboratory held a scientific and practical seminar at Karshi State University",
-            },
-            data: "07.04.2025",
-            img: 'https://picsum.photos/id/1015/400/800',
-            link: '/uz/news/1',
-        },
-        {
-            title: {
-                uz:"O‘zbekiston florasi laboratoriyasi xodimlari Qarshi davlat universitetida ilmiy-amaliy seminar o‘tkazdi",
-                ru: "Сотрудники лаборатории флоры Узбекистана провели научно-практический семинар в Каршинском государственном университете",
-                en: "Employees of the Uzbekistan Flora Laboratory held a scientific and practical seminar at Karshi State University",
-            },
-            data: "07.04.2025",
-            img: 'https://picsum.photos/id/1015/400/800',
-            link: '/uz/news/1',
-        },
-        {
-            title: {
-                uz:"O‘zbekiston florasi laboratoriyasi xodimlari Qarshi davlat universitetida ilmiy-amaliy seminar o‘tkazdi",
-                ru: "Сотрудники лаборатории флоры Узбекистана провели научно-практический семинар в Каршинском государственном университете",
-                en: "Employees of the Uzbekistan Flora Laboratory held a scientific and practical seminar at Karshi State University",
-            },
-            data: "07.04.2025",
-            img: 'https://picsum.photos/id/1015/400/800',
-            link: '/uz/news/1',
-        },
-        {
-            title: {
-                uz:"O‘zbekiston florasi laboratoriyasi xodimlari Qarshi davlat universitetida ilmiy-amaliy seminar o‘tkazdi",
-                ru: "Сотрудники лаборатории флоры Узбекистана провели научно-практический семинар в Каршинском государственном университете",
-                en: "Employees of the Uzbekistan Flora Laboratory held a scientific and practical seminar at Karshi State University",
-            },
-            data: "07.04.2025",
-            img: 'https://picsum.photos/id/1015/400/800',
-            link: '/uz/news/1',
-        },
-        {
-            title: {
-                uz:"O‘zbekiston florasi laboratoriyasi xodimlari Qarshi davlat universitetida ilmiy-amaliy seminar o‘tkazdi",
-                ru: "Сотрудники лаборатории флоры Узбекистана провели научно-практический семинар в Каршинском государственном университете",
-                en: "Employees of the Uzbekistan Flora Laboratory held a scientific and practical seminar at Karshi State University",
-            },
-            data: "07.04.2025",
-            img: 'https://picsum.photos/id/1015/400/800',
-            link: '/uz/news/1',
-        },
-        {
-            title: {
-                uz:"O‘zbekiston florasi laboratoriyasi xodimlari Qarshi davlat universitetida ilmiy-amaliy seminar o‘tkazdi",
-                ru: "Сотрудники лаборатории флоры Узбекистана провели научно-практический семинар в Каршинском государственном университете",
-                en: "Employees of the Uzbekistan Flora Laboratory held a scientific and practical seminar at Karshi State University",
-            },
-            data: "07.04.2025",
-            img: 'https://picsum.photos/id/1015/400/800',
-            link: '/uz/news/1',
-        },
-        {
-            title: {
-                uz:"O‘zbekiston florasi laboratoriyasi xodimlari Qarshi davlat universitetida ilmiy-amaliy seminar o‘tkazdi",
-                ru: "Сотрудники лаборатории флоры Узбекистана провели научно-практический семинар в Каршинском государственном университете",
-                en: "Employees of the Uzbekistan Flora Laboratory held a scientific and practical seminar at Karshi State University",
-            },
-            data: "07.04.2025",
-            img: 'https://picsum.photos/id/1015/400/800',
-            link: '/uz/news/1',
-        },
-        ]
+
+    useEffect(() => {
+        const api = getApiUrl();
+
+        for (let index = 0; index < dataType.length; index++) {
+            async function fetchAll() {
+                try {
+                    const response = await axios.post(`${api}/api${dataType[index].link}/get-item`, { page: 0 });
+                    if (response.data.success) {
+                        if (dataType[index].link === '/news') {
+                            const formattedData = response.data.data.map(item => ({
+                                ...item,
+                                tupe: dataType[index].title,
+                                typeof: 'news',
+                            }));
+                            setNews(formattedData);
+                        } else if (dataType[index].link === '/ads') {
+                            const formattedData = response.data.data.map(item => ({
+                                ...item,
+                                tupe: dataType[index].title,
+                                typeof: 'ads',
+                            }));
+                            setAds(formattedData);
+                        } else if (dataType[index].link === '/events') {
+                            const formattedData = response.data.data.map(item => ({
+                                ...item,
+                                tupe: dataType[index].title,
+                                typeof: 'event',
+                            }));
+                            setEvents(formattedData);
+                        }
+                    } else {
+                        console.error('Ошибка при получении элементов :', response.data.message);
+                    }
+                } catch (error) {
+                    console.error('Ошибка при запросе элементов :', error);
+                }
+            }
+            fetchAll();
+        }
+
+        
+
+        // if (dataLenght <= 16) {
+
+            // let data = [];
+            // data.push(...news);
+            // data.push(...ads);
+            // data.push(...events);
+
+            // setNewsListData(data);
+
+            // console.log('newsListData', data);
+            
+            
+        // }
+        
+    }, []);
+
+
+    useEffect(() => {
+        let result = [];
+        let n = 0, a = 0, e = 0;
+        while (result.length < 12 && (news.length > n || ads.length > a || events.length > e)) {
+            // 5 news
+            for (let i = 0; i < 5 && n < news.length && result.length < 12; i++) {
+                result.push(news[n++]);
+            }
+            // 2 ads
+            for (let i = 0; i < 2 && a < ads.length && result.length < 12; i++) {
+                result.push(ads[a++]);
+            }
+            // 1 event
+            if (e < events.length && result.length < 12) {
+                result.push(events[e++]);
+            }
+        }
+        setNewsListData(result);
+        console.log(result);
+        
+    }, [news, ads, events]);
+
+
+    function formatDate(dateStr) {
+        if (!dateStr) return '';
+        const date = new Date(dateStr);
+        if (isNaN(date)) return '';
+        const day = String(date.getDate()).padStart(2, '0');
+        const month = String(date.getMonth() + 1).padStart(2, '0');
+        const year = date.getFullYear();
+        return `${day}.${month}.${year}`;
+    }
+
   return (
     <div className='home-page__news'>
         {
-            data?.map((item, index) => (
+            newsListData?.map((item, index) => (
                 <div className='home-page__news__item' key={index}>
-                    <Link className='home-page__news__item__link' to={item.link}>
+                    <Link className='home-page__news__item__link' to={`${item.typeof}/${item.link}`}>
                         <div className='home-page__news__item__img-wrapper'>
-                            <img src={item.img} alt="" className='home-page__news__item__img' />
+                            <img src={item.image} alt="" className='home-page__news__item__img' />
                         </div>
                     </Link>
 
                     <div className='home-page__news__item__type_link'>
-                        <Link to={item}>
-                            Yangiliklar
+                        <Link to={item.typeof}>
+                            {
+                                item.tupe[language]
+                            }
                         </Link>
                     </div>
 
-                    <p className='home-page__news__item__data'> <i className="fa-regular fa-calendar-days"></i>  {item.data}</p>
+                    <p className='home-page__news__item__data'> <i className="fa-regular fa-calendar-days"></i>  {formatDate(item.date)}</p>
                     
-                    <Link className='home-page__news__item__link' to={item.link}>
+                    <Link className='home-page__news__item__link' to={`${item.typeof}/${item.link}`}>
                         <h1 className='home-page__news__item__title'>{item.title[language]}</h1>
                     </Link>
                 </div>

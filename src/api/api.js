65f99bc779309f29428bb03837b8de54106662c4
SayfullaevSirojiddin <-email@example.com>
2025-06-23 15:07:27 +0500

    commit

--- src/api/api.js
+++ src/api/api.js
@@ -1,6 +1,6 @@
-const api = 'http://localhost:3108'
+// const api = 'http://localhost:3109'
 
-// const api = 'https://milliy-arxeologiya-markazi-admin-api-1.onrender.com';
+const api = 'https://milliy-arxeologiya-markazi-admin-api-1.onrender.com';
 
 
 export default function getApiUrl() {
